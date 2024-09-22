7756191e2693808cc44fe08c5faffe231eb18878
nitishsharma2825 <sharnit2825@gmail.com>
2024-09-22 21:26:32 +0530

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -2,17 +2,20 @@
 #include <string.h>
 
 int main() {
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-  fflush(stdout);
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  
-  input[strlen(input) - 1] = '\0';
-  printf("%s: command not found\n", input);
-  fflush(stdout);
+  while (1)
+  {
+    printf("$ ");
+    fflush(stdout);
+
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
+    
+    input[strlen(input) - 1] = '\0';
+    printf("%s: command not found\n", input);
+    fflush(stdout);
+  }
 
   return 0;
 }
