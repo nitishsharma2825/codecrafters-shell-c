998a7f6cf6231af876d65db07046e6f68cfe11c9
nitishsharma2825 <sharnit2825@gmail.com>
2024-09-22 23:24:55 +0530

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main() {
 
-  char *built_in_commands[] = {"echo", "type", "cat", "exit"};
+  char *built_in_commands[] = {"echo", "type", "exit"};
   int numOfCommands = sizeof(built_in_commands) / sizeof(built_in_commands[0]);
 
   while (1)
@@ -33,22 +34,38 @@ int main() {
 
       char *commandType = strtok(NULL, " ");
       int found = 0;
+
       for (size_t i = 0; i < numOfCommands; i++) {
-
         if (strcmp(commandType, built_in_commands[i]) == 0) {
-
           found = 1;
           printf("%s is a shell builtin\n", commandType);
           break;
+        }
+      }
+      
+      char *path = getenv("PATH");
+      char *path_copy = strdup(path);
 
+      char *dir = strtok(path_copy, ":");
+      char full_path[1024];
+      while (dir!=NULL) {
+        if (found == 1)
+        {
+          break;
         }
         
+        sprintf(full_path, "%s/%s", dir, commandType);      
+        if (access(full_path, X_OK) == 0 )
+        {
+          found = 1;
+          printf("%s is %s\n", commandType, full_path);
+          break;
+        }
+        dir = strtok(NULL, ":");
       }
 
       if (!found) {
-
-        printf("%s not found\n", commandType);
-
+          printf("%s: not found\n", commandType);
       }
 
     } else {
