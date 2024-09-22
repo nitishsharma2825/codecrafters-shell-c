efc5868d326a65f7a531f3d4e1177c6f0eeff778
nitishsharma2825 <sharnit2825@gmail.com>
2024-09-22 21:40:38 +0530

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -14,10 +14,16 @@ int main() {
     fgets(input, 100, stdin);
 
     input[strlen(input) - 1] = '\0';
+    char *command = strdup(input);
+    char *commandType = strtok(command, " ");
+
     if (strcmp(input, "exit 0") == 0) {
       exit(0);
+    } else if (strcmp(commandType, "echo") == 0) {
+      printf("%s\n", input+5);
+    } else {
+      printf("%s: command not found\n", input);
     }
-    printf("%s: command not found\n", input);
     fflush(stdout);
   }
 
