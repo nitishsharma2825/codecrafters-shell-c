fc80caf27638c17e66b3784b635986971089ff45
nitishsharma2825 <sharnit2825@gmail.com>
2024-09-22 21:31:04 +0530

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main() {
@@ -11,8 +12,11 @@ int main() {
     // Wait for user input
     char input[100];
     fgets(input, 100, stdin);
-    
+
     input[strlen(input) - 1] = '\0';
+    if (strcmp(input, "exit 0") == 0) {
+      exit(0);
+    }
     printf("%s: command not found\n", input);
     fflush(stdout);
   }
